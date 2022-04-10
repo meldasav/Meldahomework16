ced83fb896c40bcaac1e6aff45ba76dca5158794
meldasav <gmeldag83@hotmail.com>
2022-04-09 23:26:49 -0500

    I am still working on it

--- src/homeworks/Homework_16.java
+++ src/homeworks/Homework_16.java
@@ -4,6 +4,7 @@ import java.util.*;
 
 public class Homework_16 {
     public static void main(String[] args) {
+
         System.out.println(countWords("     Java is fun       "));//TASK1
         System.out.println(countWords("Selenium is the most common UI automation tool.   "));//TASK1
         System.out.println(countWords(""));//TASK1
@@ -13,6 +14,10 @@ public class Homework_16 {
         System.out.println(countPos(new ArrayList<>(Arrays.asList(-45, 0, 0, 34, 5, 67))));//TASK3
         System.out.println(countPos(new ArrayList<>(Arrays.asList(-23, -4, 0, 2, 5, 90, 123))));//TASK3
         System.out.println(removeExtraSpaces("   I   am      learning     Java      "));
+        System.out.println(removeDuplicateNumbers(new ArrayList(Arrays.asList(10, 20, 35, 20, 35, 60, 70, 60))));//TAsk4
+        System.out.println(removeDuplicateNumbers(new ArrayList(Arrays.asList(1, 2, 5, 2, 3))));//TAsk4
+        System.out.println(removeDuplicateElements(new ArrayList<>(Arrays.asList("java", "C#", "ruby", "JAVA", "ruby", "C#", "C++"))));//TASK5
+        System.out.println(Arrays.toString(add(new int[]{3, 0, 0, 7, 5, 10}, new int[]{6, 3, 2})));//TASK6
     }
 
     //TASK1
@@ -35,23 +40,61 @@ public class Homework_16 {
         }
         return count;
     }
+
     //TASK3
-    public static Integer countPos(List<Integer> numbers){
+    public static Integer countPos(List<Integer> numbers) {
         int positive = 0;
         for (Integer number : numbers) {
             if (number > 0) {
                 positive++;
             }
         }
-                return positive;
-            }
-            //TASK6
-            static String removeExtraSpaces(String str)
-            {
-                str = str.replaceAll(" +"," ");
-                return str;
+        return positive;
+    }
+    //TASK4
+
+    public static ArrayList<Integer> removeDuplicateNumbers(ArrayList<Integer> numbers) {
+        return new ArrayList<>(new HashSet<>(numbers));
+    }
+
+    //TASK5
+    public static List<String> removeDuplicateElements(ArrayList<String> str) {
+//        List<String> str1 = new ArrayList<>(str);
+//        str1.removeIf(x->x.equalsIgnoreCase(String.valueOf(str)));
+//        return str1;
+        List<String> str1 = new ArrayList<>(str);
+        for (String s : str1) {
+            if (!str1.contains(s)) {
+                str1.add(s);
             }
         }
+        return str1;
+    }
+
+    //TASK6
+    public static String removeExtraSpaces(String str) {
+        str = str.replaceAll(" +", " ");
+        return str;
+    }
+
+    //TASK7
+    public static int[] add(int[] a, int[] b) {
+        int len = Math.min(a.length, b.length);
+        int[] c = new int[len];
+        for (int i = 0; i < len; i++) {
+            c[i] = a[i] + b[i];
+        }
+        return c;
+    }
+}
+//       //TASK8
+//  //  public static int findClosestTo10(int[] number){
+//    }
+//}
+
+
+
+
 
 
 
